4aa13b35e666db6491b639743f13c8bbcab918e6
Xavier Lin <xavier.lin.1332@gmail.com>
2018-09-08 11:08:42 -0400

    Add project files.

---
+++ WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        
+
+        
+
+        private void clicked_Click(object sender, RoutedEventArgs e)
+        {
+            System.Random rnd = new System.Random();
+            int RandomOutput = rnd.Next(1,100);
+            string input = Input.Text;
+            int.TryParse(input, out int TrueOutput);
+            if (RandomOutput == TrueOutput)
+            {
+                Winner.Text = "WINNER!!!!!!!";
+                Winner.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Winner.Text = "You lose. Try again.";
+            }
+           
+        }
+    }
+}
---
+++ guessnumbertest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("guessnumbertest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("guessnumbertest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("78dc5c2b-8304-4d29-8413-1df8a96121e5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
